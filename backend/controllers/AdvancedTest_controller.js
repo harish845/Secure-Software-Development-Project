037ef42134f3f9de0bde947db4b92bc935e6c64d
Harish <129611646+Harish3000@users.noreply.github.com>
2024-09-15 12:05:16 +0530

    Fix SSRF Vulnerability

--- backend/controllers/AdvancedTest_controller.js
+++ backend/controllers/AdvancedTest_controller.js
@@ -1,13 +1,20 @@
-// AdvancedTest_controller.js
 const axios = require("axios");
 
 // Azure Custom Vision API Endpoint and Prediction Key
 const predictionEndpoint = process.env.SIGHT_PREDICTION_ENDPOINT;
 const predictionKey = process.env.SIGHT_PREDICTION_KEY;
 
+// Whitelist of allowed endpoints
+const allowedEndpoints = [predictionEndpoint];
+
 // Controller function to upload and analyze the image
 const uploadImage = async (req, res) => {
   try {
+    // Ensure the endpoint is in the whitelist
+    if (!allowedEndpoints.includes(predictionEndpoint)) {
+      throw new Error("Endpoint not allowed.");
+    }
+
     // Send the image to the Azure Custom Vision API
     const response = await axios.post(predictionEndpoint, req.file.buffer, {
       headers: {
