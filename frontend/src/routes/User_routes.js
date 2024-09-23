7e873070c0c93ead4f34b32b7a5cc40d71b0b49c
Pathum Dissanayake <100502665+Pathumdissanayake@users.noreply.github.com>
2024-09-23 08:50:11 +0530

    Route protection

--- frontend/src/routes/User_routes.js
+++ frontend/src/routes/User_routes.js
@@ -7,19 +7,41 @@ import UserProfile from "../components/User_components/UserProfile";
 import UpdateUser from "../components/User_components/UpdateUser";
 import AdminDashBoard from "../components/User_components/AdminDashBoard";
 import OAuthCallback from "../components/User_components/OAuthCallback";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function User_routes() {
   return (
     <Routes>
       <Route>
-      <Route path="/" element={< ProfileHome/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/home" element={<UserProfile/>} />
-      <Route path="/update" element={<UpdateUser/>} />
-     
-      <Route path="/oauth-callback" element={<OAuthCallback/>} />
-      <Route path="/admin-dashboard" element={<AdminDashBoard/>} />
+        <Route path="/" element={<ProfileHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/oauth-callback" element={<OAuthCallback />} />
+        {/* Protected routes */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute requiredRole="user">
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/update"
+          element={
+            <ProtectedRoute requiredRole="user">
+              <UpdateUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin-dashboard"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminDashBoard />
+            </ProtectedRoute>
+          }
+        />
       </Route>
     </Routes>
   );
